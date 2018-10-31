8c0ea4baa47c4b5642831fef7e7359c45ae11cb5
Samin Bin Karim <saminbinkarim@gmail.com>
2018-10-31 13:56:40 +0000

    after exam 2 Full functionality added

--- script.js
+++ script.js
@@ -10,62 +10,7 @@ var moviecart = localStorage.getItem('moviecart') ? JSON.parse(localStorage.getI
 localStorage.setItem('moviecart', JSON.stringify(moviecart)); 
 
 
-function loadMovies(movie, index) {
-    var colDiv = $('<div>').addClass('column');
-    var screenTimesDiv = $('<div>').addClass('mvST');
-    var nameDiv = $('<div>').addClass('mvName');
-    var buttonDiv = $('<div>').addClass('mvBtn');
-
-    screenTimesDiv.append($('<p>').text("Screen Times"));
-    screenTimesDiv.append($('<p>').text(movie.st));
-    
-    nameDiv.append($('<p>').text(movie.name))
-
-    var changeStatebtn = $('<button>').addClass('btn').text(movie.state).attr('id', index);
-    buttonDiv.append(changeStatebtn);
-
-    changeStatebtn.click(function(event) {
-      if (event.target.innerText == "upcoming") {
-        moviecart[event.target.id].state = "show next";
-        var idd = "#" + event.target.id;
-        $(idd).css("background-color", "yellow");
-        event.target.innerText = "show next";
-      }
-      else if (event.target.innerText == "show next") {
-          moviecart[event.target.id].state = "screening";
-          var idd = "#" + event.target.id;
-          $(idd).css("background-color", "cyan");
-          
-          event.target.innerText = "screening";
-      } else {
-        moviecart[event.target.id].state = "upcoming";
-        moviecart[event.target.id].st = (parseInt(moviecart[event.target.id].st) + 1);
-        var idd = "#" + event.target.id;
-        $(idd).css("background-color", "green");
-        event.target.innerText = "upcoming";
-      }
-      localStorage.setItem('moviecart', JSON.stringify(moviecart));
-    });
-    // colDiv.css("float","left");
-    // colDiv.css("width, 100%");
-    // screenTimesDiv.css("float","left");
-    // screenTimesDiv.css("width, 30%");
-    // nameDiv.css("float","left");
-    // nameDiv.css("width, 40%");
-    // buttonDiv.css("float","left");
-    // buttonDiv.css("width, 30%");
-    
-    colDiv.append(screenTimesDiv);
-    colDiv.append(nameDiv);
-    colDiv.append(buttonDiv);
-    $('#movieShow').append(colDiv);
-
-  }
-
-
-$(document).ready(function() {
-
-
+function loadMovies() {
   moviecart.forEach(function(movie, index) {
     var colDiv = $('<div>').addClass('column');
     var screenTimesDiv = $('<div>').addClass('mvST');
@@ -73,7 +18,9 @@ $(document).ready(function() {
     var buttonDiv = $('<div>').addClass('mvBtn');
 
     screenTimesDiv.append($('<p>').text("Screen Times"));
-    screenTimesDiv.append($('<p>').text(movie.st));
+    
+    var temp = "st" + index;  
+    screenTimesDiv.append($('<p>').text(movie.st).attr("id",temp));
     
     nameDiv.append($('<p>').text(movie.name))
 
@@ -99,9 +46,14 @@ $(document).ready(function() {
         var idd = "#" + event.target.id;
         $(idd).css("background-color", "green");
         event.target.innerText = "upcoming";
+        idd = "#st" + event.target.id;
+        $(idd).text(moviecart[event.target.id].st);
       }
       localStorage.setItem('moviecart', JSON.stringify(moviecart));
     });
+    
+    
+    
     colDiv.css("float","left");
     colDiv.css("width", "100%");
     screenTimesDiv.css("float","left");
@@ -117,18 +69,22 @@ $(document).ready(function() {
     $('#movieShow').append(colDiv);
 
   });
+}
 
+$(document).ready(function() {
+  loadMovies();
   $("#addMovieButton").click(function(event) {
     namee = $("#movieTextBox").val();
     if (namee == ""){
+      alert("Movie name cannot be empty");
       return;
     }
     obj = { name: "hasdaooasd", st: "0", state: "upcoming" };
     obj.name = namee;
     moviecart.push(obj);
     localStorage.setItem('moviecart', JSON.stringify(moviecart));
-    //document.getElementById('#movieTextBox').value = "";
-    console.log(moviecart);
+    loadMovies();
+    //console.log(moviecart);
   });
 
 
